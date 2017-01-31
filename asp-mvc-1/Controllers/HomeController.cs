b51e3d233ecb2073c46358543f3e6660a6c1a2c2
pradanadike <pradanadike5@gmail.com>
2017-01-31 11:13:01 +0700

    Add project files.

---
+++ asp-mvc-1/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+namespace asp_mvc_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
